7f497d863ba828f1cfba2cf86575c83355fbb8d1
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-01-23 18:57:08 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");// \은 개행 문자 \\로 적어야 출력
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+    }
+}
