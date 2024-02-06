64e6668ec03439a6cb62868d0186b32686b810e0
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-02-06 16:39:38 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result =0;
+        for(int i = 1;i <= N;i++ ){
+            result +=i;
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
